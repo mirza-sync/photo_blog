daca725f38818c57d87119184bb799b9b7ad72c7
mirza-sync <mirzasyahmi123@gmail.com>
2023-09-21 23:30:28 +0800

    Add hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import Hero from "./components/Hero";
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <Hero
+        heading={"Next Photography"}
+        message={"Envisioning the next level photography"}
+      />
+    </div>
+  );
 }
