9c1b703432dad1c85dc33512c4db6ffcdd765a6f
mirza-sync <mirzasyahmi123@gmail.com>
2023-09-23 23:34:54 +0800

    Feat: Toggle navbar on mobile

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
 function Navbar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <div className="fixed left-0 top-0 z-10 w-full duration-300 ease-in">
       <div className="m-auto flex max-w-[1240px] items-center justify-between p-4 text-white">
@@ -23,12 +33,22 @@ function Navbar() {
         </ul>
 
         {/* Mobile menu toggle button */}
-        <div className="z-10 block sm:hidden">
-          <AiOutlineMenu size={20} />
+        <div onClick={toggleNav} className="z-10 block sm:hidden">
+          {isNavOpen ? (
+            <AiOutlineClose size={20} />
+          ) : (
+            <AiOutlineMenu size={20} />
+          )}
         </div>
 
         {/* Mobile menu */}
-        <div className="absolute bottom-0 left-0 right-0 top-0 flex h-screen w-full items-center justify-center bg-black text-center duration-300 ease-in sm:hidden">
+        <div
+          className={
+            isNavOpen
+              ? "absolute bottom-0 left-0 right-0 top-0 flex h-screen w-full items-center justify-center bg-black text-center duration-300 ease-in sm:hidden"
+              : "absolute bottom-0 left-[-100%] right-0 top-0 flex h-screen w-full items-center justify-center bg-black text-center duration-300 ease-in sm:hidden"
+          }
+        >
           <ul>
             <li className="p-4 text-4xl hover:text-gray-500">
               <Link href="/">Home</Link>
