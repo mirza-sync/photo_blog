2aea19472f6c0d22dc53aaed5fc2366431823d89
mirza-sync <mirzasyahmi123@gmail.com>
2023-09-23 23:36:07 +0800

    Refactor: Remove unnecessary margin

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero({ heading, message }: HeroProps) {
     <div className="custom-img mb-12 flex h-screen items-center justify-center bg-cover bg-fixed bg-center">
       {/* Overlay */}
       <div className="absolute bottom-0 left-0 right-0 top-0 z-[2] bg-black/70" />
-      <div className="z-[2] ml-[-10rem] mt-[-10rem] p-5 text-white">
+      <div className="z-[2] mt-[-10rem] p-5 text-white">
         <h2 className="text-5xl font-bold">{heading}</h2>
         <p className="py-5 text-xl">{message}</p>
         <button className="border px-8 py-2">Book</button>
