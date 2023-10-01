a4086f687e420b59b9ad8c3d06f43a97a4b1b1c9
mirza-sync <mirzasyahmi123@gmail.com>
2023-10-02 01:06:25 +0800

    Add ``work` content

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Hero from "../components/Hero";
+import { Portfolio } from "../components/Portfolio";
 
 export default function Work() {
-  return <div>Work</div>;
+  return (
+    <div>
+      <Hero
+        heading={"My Work"}
+        message={
+          "These are some of the scenes I captured when travelling around the world"
+        }
+      />
+      <Portfolio />
+    </div>
+  );
 }
